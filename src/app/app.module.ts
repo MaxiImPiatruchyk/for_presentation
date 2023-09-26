7acc3b7e4ce8171a61df031f450bfb5e2ef13738
MaksimP <maksim.piatruchyk@gmail.com>
2023-09-26 08:47:52 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WizardStepBarComponent } from './components/wizard-step-bar/wizard-step-bar.component';
+import { ContentComponent } from './components/content/content.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardStepBarComponent,
+    ContentComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
